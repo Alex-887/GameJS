3c7a7b5567d95fb6357a8bcbd02d12e9b344a1e5
LudoSa <ludovic.sahraoui@gmail.com>
2020-03-03 16:33:21 +0100

    Game

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
+"use strict";
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+canvas.width = 1920;
+canvas.height = 1080;
 document.body.appendChild(canvas);
 
 // Background image
@@ -11,7 +12,7 @@ var bgImage = new Image();
 bgImage.onload = function () {
 	bgReady = true;
 };
-bgImage.src = "images/background.png";
+bgImage.src = "Ressources/Images/Map.png";
 
 // Hero image
 var heroReady = false;
@@ -19,7 +20,7 @@ var heroImage = new Image();
 heroImage.onload = function () {
 	heroReady = true;
 };
-heroImage.src = "images/hero.png";
+heroImage.src = "Ressources/Images/trump.jpg";
 
 // Monster image
 var monsterReady = false;
@@ -27,13 +28,15 @@ var monsterImage = new Image();
 monsterImage.onload = function () {
 	monsterReady = true;
 };
-monsterImage.src = "images/monster.png";
+monsterImage.width=32
+monsterImage.height =32
+monsterImage.src = "Ressources/Images/mexicanos.jpeg";
 
 // Game objects
 var hero = {
 	speed: 256 // movement in pixels per second
 };
-var monster = {};
+var monster = { speed: 128};
 var monstersCaught = 0;
 
 // Handle keyboard controls
@@ -49,12 +52,18 @@ addEventListener("keyup", function (e) {
 
 // Reset the game when the player catches a monster
 var reset = function () {
+
 	hero.x = canvas.width / 2;
 	hero.y = canvas.height / 2;
 
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
+
+
+        // Throw the monster somewhere on the screen randomly
+         monster.x = 32 + (Math.random() * (canvas.width - 64));
+	     monster.y = 32 + (Math.random() * (canvas.height - 64));
+
+
+
 };
 
 // Update game objects
@@ -80,10 +89,45 @@ var update = function (modifier) {
 		&& monster.y <= (hero.y + 32)
 	) {
 		++monstersCaught;
-		reset();
+        mexicansNumber--;
+        //alert("You are hit !")
+		//reset();
 	}
 };
 
+//Move mexicans
+var redrawMexicans = function(modifier){
+
+   // monster.x=monster.x - 1;
+    var diffx = Math.floor(hero.x - monster.x);
+    var diffy = Math.floor(hero.y - monster.y);
+
+    //Distance min between trump and mexicans
+    var gap = 20;
+
+
+    //Set movement of the ennemies
+    if (diffy < -gap) { // Player holding up
+		monster.y -= monster.speed * modifier;
+	}
+	if (diffy > gap) { // Player holding down
+		monster.y += monster.speed * modifier;
+	}
+	if (diffx < -gap) { // Player holding left
+		monster.x -= monster.speed * modifier;
+	}
+	if (diffx > gap) { // Player holding right
+		monster.x += monster.speed * modifier;
+	}
+
+
+    if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+}
+
+var mexicansNumber = 0
 // Draw everything
 var render = function () {
 	if (bgReady) {
@@ -94,27 +138,37 @@ var render = function () {
 		ctx.drawImage(heroImage, hero.x, hero.y);
 	}
 
-	if (monsterReady) {
+    while(mexicansNumber < 5){
+        if (monsterReady) {
 		ctx.drawImage(monsterImage, monster.x, monster.y);
-	}
+        }
+    mexicansNumber++;
+    }
+
 
 	// Score
-	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.fillStyle = "rgb(250, 150, 150)";
 	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+	//ctx.textAlign = "left";
+	//ctx.textBaseline = "top";
+	ctx.fillText("Mexicans caught: " + monstersCaught, 0, 200);
+
+
+
 };
 
 // The main game loop
 var main = function () {
 	var now = Date.now();
+    //Stabilize the speed of entities
 	var delta = now - then;
 
 	update(delta / 1000);
 	render();
 
 	then = now;
+
+    setInterval(redrawMexicans(delta / 1000), 1000)
 
 	// Request to do this again ASAP
 	requestAnimationFrame(main);
